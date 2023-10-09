a67266a6d551398ddf034dd403e103ca3fb8b91d
Zavqiddin-tech <zavqiddinergashov2002@gmail.com>
2023-10-09 17:26:19 +0500

    prices qismi qilindi lekin ichi bo'sh

---
+++ src/views/prices/prices.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="prices">
+        <div class="container">
+            <h2>prices</h2>
+        </div>
+    </div>
+</template>
+
+<script setup>
+</script>
+
+<style lang="scss">
+    
+</style>
